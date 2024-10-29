a282305fa1d457630af947c43a54138c7333c0c6
AndryAlexis <andry.html@gmail.com>
2024-10-29 11:55:48 +0100

    MONGO_URL added to .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
 
-const IP = '127.0.0.1'
-const DB_PORT = '27017'
-
-mongoose.connect(`mongodb://${IP}:${DB_PORT}/online-shop`)
+mongoose.connect(process.env.MONGO_URL)
 // mongoose.connect(`mongodb+srv://root:mongodbpassword@cluster1.cc8gu.mongodb.net/`)
